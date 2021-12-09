c647773a629693b0cc43b86f34b1fddab74be52c
thanhphuocdang <thanhphuoc0209@gmail.com>
2021-12-09 16:40:40 +0700

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,33 @@
-import { Component } from "@angular/core";
+import {
+  Component,
+  ElementRef,
+  Injector,
+  TemplateRef,
+  ViewChild,
+  ViewContainerRef,
+} from '@angular/core';
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html"
+  selector: 'app-root',
+  templateUrl: './app.component.html',
 })
-export class AppComponent {}
+export class AppComponent {
+  isTh = true;
+  las = 10;
+  ps = [25, 75];
+  ref: ElementRef<HTMLElement>;
+  ins: Injector;
+  @ViewChild('plit') container: ViewContainerRef;
+  changeT() {
+    this.isTh = !this.isTh;
+    this.ps = [75, 25];
+    // this.container.nativeElement.setAttribute('a', 'a');
+    let e = this.container.injector.get(HTMLElement);
+    console.log(e);
+  }
+  constructor(private injector: Injector) {}
+}
+
+function plit(plit: any) {
+  throw new Error('Function not implemented.');
+}
